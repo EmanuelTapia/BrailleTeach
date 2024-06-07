5584892fe6bc778e0f06dea9e967463bc21ac41d
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-06-06 20:23:08 -0500

    commit index 3

---
+++ js/index.js
@@ -0,0 +1,13 @@
+function btnIndex(){
+    location.assign("../inicio.html");
+}
+
+function btnIniciar(){
+    location.assign("../login.html");
+}
+
+document.addEventListener("keydown", function(event) {
+    if (event.key === " ") { // Verificar si se presionó la tecla espacio
+        location.assign("../login.html");
+    }
+  });
--- js/voz.js
+++ js/voz.js
@@ -2,6 +2,7 @@ function mensaje(id) {
     const div = document.getElementById(id).textContent;
     const mensaje = new SpeechSynthesisUtterance(div);
     mensaje.rate=1.5;
+    mensaje.pitch=2;
     window.speechSynthesis.speak(mensaje);
   }
 
