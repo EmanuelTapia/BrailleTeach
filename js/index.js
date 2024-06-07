8f776fd81d0a92d2e3ee2bd352d1061406755cb9
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-06-07 12:06:51 -0500

    commit index 4

--- js/index.js
+++ js/index.js
@@ -1,13 +1,10 @@
-function btnIndex(){
-    location.assign("../inicio.html");
-}
 
 function btnIniciar(){
-    location.assign("../login.html");
+    location.assign("./login.html");
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.key === " ") { // Verificar si se presionó la tecla espacio
-        location.assign("../login.html");
+        location.assign("./login.html");
     }
   });
