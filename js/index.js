413f3705fcc07abb88f826bbbc85ed0fab0e2fe3
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-06-07 12:14:53 -0500

    Update index.js 1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,10 @@
 
 function btnIniciar(){
-    location.assign("./login.html");
+    location.assign("../login.html");
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.key === " ") { // Verificar si se presionó la tecla espacio
-        location.assign("./login.html");
+        location.assign("../login.html");
     }
   });
