be020b2145db44a1b9b585073d5550b85cd05980
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-06-11 05:31:32 -0500

    commit aprender 1

--- js/index.js
+++ js/index.js
@@ -1,10 +1,16 @@
 
 function btnIniciar(){
     location.assign("./login.html");
+    speechSynthesis.cancel();
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.key === " ") { // Verificar si se presionó la tecla espacio
         location.assign("./login.html");
+        speechSynthesis.cancel();
     }
   });
+
+import { mensaje } from './voz.js';
+
+mensaje("bienvenido")
