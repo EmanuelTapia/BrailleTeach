aee042e2e7a6e7851e7d1f7b49a71e1c86a432be
EmanuelTapia <166262685+EmanuelTapia@users.noreply.github.com>
2024-07-02 15:35:58 -0500

    commit tutorial 1

--- js/index.js
+++ js/index.js
@@ -1,12 +1,12 @@
 
 function btnIniciar(){
-    location.assign("./login.html");
+    location.assign("./tutorial.html");
     speechSynthesis.cancel();
 }
 
 document.addEventListener("keydown", function(event) {
     if (event.key === " ") { // Verificar si se presionó la tecla espacio
-        location.assign("./login.html");
+        location.assign("./tutorial.html");
         speechSynthesis.cancel();
     }
   });
